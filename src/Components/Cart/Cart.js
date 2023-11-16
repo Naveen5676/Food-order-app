30dfd4bca9c676fc417d7dff7791063d096aa716
Naveen5676 <sample.first@yahoo.com>
2023-11-16 19:31:25 +0530

    Making a hard Coded Cart without any Functionality

---
+++ src/Components/Cart/Cart.js
@@ -0,0 +1,35 @@
+import React from "react";
+import classes from "./Cart.module.css";
+import Modal from "../UI/Modal";
+const Cart = (props) => {
+  const cartitems = (
+    <ul className={classes["cart-items"]}>
+      {[
+        {
+          id: "c1",
+          name: "sushi",
+          amount: 24.0,
+          price: 12,
+        },
+      ].map((item) => (
+        <li>{item.name}</li>
+      ))}
+    </ul>
+  );
+
+  return (
+    <Modal>
+      {cartitems}
+      <div className={classes.total}>
+        <span>Total Amount</span>
+        <span> 24.26</span>
+      </div>
+      <div className={classes.actions}>
+        <button className={classes["button--alt"]}>Close</button>
+        <button className={classes.button}>Order</button>
+      </div>
+    </Modal>
+  );
+};
+
+export default Cart;
--- src/Components/Layout/Header-cart-button.js
+++ src/Components/Layout/Header-cart-button.js
@@ -6,7 +6,7 @@ const HeaderCartButton =()=>{
 return (
     <button className={classes.button}>
         <span className={classes.icon}>
-            <CartIcon/>
+            <CartIcon />
         </span>
         <span>Your Cart</span>
         <span className={classes.badge}>1</span>
